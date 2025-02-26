a985f57986da3d44875f0745c021b2e6effb378c
H82M19\Student <Student>
2025-02-26 17:23:10 +0700

    UPDATE: Bai1 & Bai2

--- NguyenTranGiaSi_22000715_LAB02/src/App.jsx
+++ NguyenTranGiaSi_22000715_LAB02/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import './App.css'       
+import Header from './components/header'
+import Center from './components/center'
+import Footer from './components/footer'
 
 function App() {
-
   return (
     <>
-      
+      <Header />
+      <Center />
+      <Footer />
     </>
   )
 }
--- NguyenTranGiaSi_22000715_LAB02/src/components/center.jsx
+++ NguyenTranGiaSi_22000715_LAB02/src/components/center.jsx
@@ -1,8 +1,20 @@
-function Center(){
-    return(
-        <>
-        </>
-    )
+import React from 'react';
+import './center.css';   
+import leftImage from './center1.png';
+import rightImage from './center2.png';
+
+function Center() {
+  return (
+    <div className="center-container">
+      <div className="left-image-container">
+        <img class="center-image1" src={leftImage} alt="Filters" />
+      </div>
+
+      <div className="right-image-container">
+        <img src={rightImage} alt="No results" />
+      </div>
+    </div>
+  );
 }
 
 export default Center;
